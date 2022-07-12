8b0d2999b3f739b80edda2f298d2a5725d57c150
oiNeh <kgh22522252@gmail.com>
2022-07-12 20:17:17 +0900

    test: 문자열 숫자 계산 *,/,+,-  추가

--- string-calculator/src/test/java/com/kata/calculation/NumberTest.java
+++ string-calculator/src/test/java/com/kata/calculation/NumberTest.java
@@ -12,29 +12,39 @@ public class NumberTest {
         String str = "1 + 1";
 
         String result = calculator.input(str);
-
         Assertions.assertThat(result).isEqualTo("2");
     }
 
     @Test
-    void 빼기() {
-        String str = " 1 - 1 ";
+    void 더하기2() {
+        String str = "1 + 1 + 1 + 1";
+
+        String result = calculator.input(str);
+
+        Assertions.assertThat(result).isEqualTo("4");
+    }
+
+    @Test
+    void 뺴기() {
+        String str = " 1 - 1";
 
         String result = calculator.input(str);
 
         Assertions.assertThat(result).isEqualTo("0");
     }
+
     @Test
     void 곱하기() {
-        String str =  "10 * 10";
+        String str = "10 * 10";
 
         String result = calculator.input(str);
 
         Assertions.assertThat(result).isEqualTo("100");
     }
+
     @Test
     void 나누기() {
-        String str = "10 / 10 ";
+        String str = "10 / 10";
 
         String result = calculator.input(str);
 
@@ -43,8 +53,7 @@ public class NumberTest {
 
     @Test
     void 마이너스_계산() {
-        String str =  "10 + -10";
-
+        String str = "10 + -10";
         String result = calculator.input(str);
 
         Assertions.assertThat(result).isEqualTo("0");
@@ -52,27 +61,38 @@ public class NumberTest {
 
     @Test
     void 괄호_계산() {
-        String str =  "(1 + 1) * 2";
+        String str = "(1 + 1) * 2";
 
         String result = calculator.input(str);
 
         Assertions.assertThat(result).isEqualTo("4");
     }
+
     @Test
     void 복합연산_1() {
-        String str =  "10 * (10 + 5)";
+        String str = "10 * (10 + 5)";
 
         String result = calculator.input(str);
 
         Assertions.assertThat(result).isEqualTo("150");
     }
+
     @Test
     void 복합연산_2() {
-        String str =  "10 * (10 + 5) / ( -99 * (-10 + (5 * 1))";
+        String str = "10 * (10 + 5) / ( -99 * (-10 + (5 * 1)))";
 
         String result = calculator.input(str);
 
         Assertions.assertThat(result).isEqualTo("742.5");
+    }
+
+    @Test
+    void 복합연산_3() {
+        String str = "1 + ((2 - 3) * (4 * ((5 + 6) / 7 ) / 8) * 10";
+
+        String result = calculator.input(str);
+
+        Assertions.assertThat(result).isEqualTo("");
     }
 
 
