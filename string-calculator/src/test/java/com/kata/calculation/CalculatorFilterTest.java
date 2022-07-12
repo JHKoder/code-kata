2879a71066bc771ab7b065a2805942deb97f81db
oiNeh <kgh22522252@gmail.com>
2022-07-12 20:42:33 +0900

    test: 문자열 계산기 문자 숫자 판별 테스트 추가

---
+++ string-calculator/src/test/java/com/kata/calculation/CalculatorFilterTest.java
@@ -0,0 +1,28 @@
+package com.kata.calculation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("문자 숫자 계산기 구별 테스트")
+public class CalculatorFilterTest {
+
+    @Test
+    void 문자_계산기를_리턴할수있다() {
+        String str = "s + s";
+
+        Calculation result = CalculatorFilter.of(str);
+
+        Assertions.assertThat(result).isInstanceOf(Character.class);
+    }
+
+    @Test
+    void 숫자_계산기를_리턴할수있다() {
+        String str = "1 + 1";
+
+        Calculation result = CalculatorFilter.of(str);
+
+        Assertions.assertThat(result).isInstanceOf(Number.class);
+    }
+
+}
