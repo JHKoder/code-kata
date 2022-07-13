f3e9f5f27add0568ac5646086967d9d5baece3b2
oiNeh <kgh22522252@gmail.com>
2022-07-13 20:40:36 +0900

    :recycle: 숫자 계산 구조 변경

--- string-calculator/src/main/java/com/kata/calculation/OperationTree.java
+++ string-calculator/src/main/java/com/kata/calculation/OperationTree.java
@@ -1,11 +1,13 @@
 package com.kata.calculation;
 
+import com.kata.exception.CalculationFailureException;
 import com.kata.exception.CalculationSymbolException;
 import java.util.Objects;
 
 public class OperationTree {
 
     public OperationTree next = null;
+    public OperationTree left = null;
 
     public Long index;
     public double value;
@@ -34,6 +36,7 @@ public class OperationTree {
             }
 
             next = new OperationTree(index + 1);
+            next.left = this;
         } else {
             next.append(sp);
         }
@@ -41,66 +44,164 @@ public class OperationTree {
     }
 
     public void operation() {
-        //무저건 첫번쨰 index 실행
-        String[] arr = {"*", "/", "+", "-"};
-        for (int i = 0; i < 30; i++) { // 반복문 범위를 벗어 날때
-            if (this.next != null) {
-                isFindOperator("+");
-                for (int j = 0; j < 4; j++) {
-                    operation(this, arr[j]);
-                }
+        OperationTree find = findFastOperation();
+
+        if(Objects.equals(find.operator, ")")){
+            System.out.println("( ) 병합");
+            removeParentheses(find.left.left,find.left,find);
+        }else{
+            if(find.left != null) {
+                processing(find.left, find, find.next);
+            }else{
+                return ;
             }
         }
-    }
 
-    public String operation(OperationTree left, String sym) {
-
-        if (next == null) {
-            return null;
+        if(next != null){
+            operation();
         }
-
-        if (Objects.equals(operator, sym)) {
-
-            next.leftRightOperation(left);
-
-            return null;
-        }
-
-        return next.operation(this, sym);
 
     }
 
-    public void leftRightOperation(OperationTree left) {
-        double leftNum = left.value;
-        char operat = left.next.operator.charAt(0);
-        double rightNum = left.next.next.value;
-        double sum = 0;
+    //left - right 으로 병합
+    public void processing(OperationTree left, OperationTree oper, OperationTree right) {
 
-        if (priority(operat) == 3) {
-            System.out.println("곱");
-            sum = leftNum * rightNum;
-        } else if (priority(operat) == 4) {
-            System.out.println("나");
-            sum = leftNum / rightNum;
-        } else if (priority(operat) == 5) {
-            System.out.println("더");
-            sum = leftNum + rightNum;
-        } else if (priority(operat) == 6) {
-            System.out.println("뺄");
-            sum = leftNum - rightNum;
+        double sum = arithmetic(left.value, oper.operator.charAt(0), right.value);
+
+        left.value=sum;
+        if(right.next != null){
+            left.next = right.next;
+            right.next.left=left;
+        }else{
+            left.next = null;
+        }
+        oper = null;
+    }
+
+
+    // ( a )  에서 a 만 남기는
+    public OperationTree removeParentheses(OperationTree left,OperationTree find,OperationTree right){
+
+        // ( 를 a 로 변경
+        left = find;
+
+        if(right.next != null){
+            //) 뒤 에 연산자가 올경우
+            left.next=right.next;
+
+        }else{
+
+            //) 제거
+            left.next =null;
+            right = null;
         }
 
-        left.next = this.next;
-        left.value = sum;
-        System.out.println("value:" + leftNum + "," + rightNum + " sum: " + sum);
+        return left;
+    }
+
+    //"(" 을 만날떄까지의 최우선순위 구하는 함수
+    public OperationTree findLeftOperation(OperationTree find, int min, OperationTree save) {
+
+        // ) 이 left 해서 null 을 만난다면 연산자의 오류가 아닐까?
+        // ) 이 ( 을 만나기 위해 만든 메소드인대 못찾으면 오류가 일어나는게 당연한것 같다.
+
+        //반복문의 끝
+        if (Objects.equals(find.operator, "(")) {
+            //우선순위가 가장 높은 객체 리턴
+            return save;
+
+        } else {
+            //현재 연산자 가져오기
+
+            if (find.operator == null) {
+                // left 가 존재하는대 연산자가 아닐경우
+                return findLeftOperation(find.left, min, save);
+
+            } else {
+                //찾는 연산자 우선순위 넘버 가져오기
+                int priorityNum = prioritySymbol(find.operator.charAt(0));
+
+                // ( 을 찾을때까지 우선 순위 구하는 곳
+                if (min > priorityNum) {
+                    min = priorityNum;
+                    save = find;
+                }
+
+                // 우선순위가 더 있나 찾아보기
+                return findLeftOperation(find.left, min, save);
+            }
+        }
 
     }
 
 
+
+
+    public OperationTree findFastOperation() {
+        return findFastOperation(this, Integer.MAX_VALUE, this);
+    }
+
+    /**
+     * 최우선 연산이 필요한곳은 찾는 메소드
+     *
+     * @param find 재귀 하면서 비교할 값
+     * @param min  우선순위 비교 숫자
+     * @param save 우선순위가 제일 높은 객체
+     *
+     * @return 우선순위가 제일 높은 객체
+     */
+    public OperationTree findFastOperation(OperationTree find, int min, OperationTree save) {
+
+        // 다음 주소값이 존재 하면 true
+        if (find.next != null) {
+
+            //연산자가 아닐경우
+            if (find.operator == null) {
+                return findFastOperation(find.next, min, save);
+            }
+
+            //연산자의 우선순위 비교시 숫자 가져오기
+            int findNum = priority(find.operator.charAt(0));
+
+            // 연산자가 ")" 일떄
+            if (findNum == 1) {
+                // ")" 을 만났을때 "(" 을 만날떄까지 left 를 만날떄까지 반복
+                return findLeftOperation(this, Integer.MAX_VALUE, save);
+            }
+
+            // *./,+,- 연산자일떄 현제 위치 값 저장
+            if (min > findNum) {
+                min = findNum;
+                save = find;
+            }
+
+            return findFastOperation(find.next, min, save);
+        }
+
+        //다음값이 존재하지 않으면 저장했던 save 을 리턴
+        return save;
+    }
+    public double arithmetic(double left, char ch, double right) {
+        switch (ch) {
+            case '*':
+                System.out.println("*");
+                return left * right;
+            case '/':
+                System.out.println("/");
+                return left / right;
+            case '+':
+                System.out.println("+");
+                return left + right;
+            case '-':
+                System.out.println("-");
+                return left - right;
+        }
+
+        throw new CalculationFailureException();
+    }
     public boolean isFindOperator(String oper) {
         if (next != null) {
-            if(Objects.equals(operator, oper)){
-                System.out.println("return");
+            if (Objects.equals(operator, oper)) {
                 return true;
             }
             return next.isFindOperator(oper);
@@ -120,25 +221,39 @@ public class OperationTree {
         }
     }
 
-    //우선순위 검색
-    public int priority(char operator) {
+    public int prioritySymbol(char operator) {
+
+        //( 을 무시한다
+        //) 가 나온다면
         switch (operator) {
-            case '(':
-                return 1;
-            case ')':
-                return 2;
             case '*':
-                return 3;
+                return 1;
             case '/':
-                return 4;
+                return 2;
             case '+':
-                return 5;
+                return 3;
             case '-':
-                return 6;
+                return 4;
         }
-        throw new CalculationSymbolException();
+        return 10;
     }
 
+    public int priority(char operator) {
+
+        switch (operator) {
+            case ')':
+                return 1;
+            case '*':
+                return 2;
+            case '/':
+                return 3;
+            case '+':
+                return 4;
+            case '-':
+                return 5;
+        }
+        return 10;
+    }
 
     private boolean isParentheses(String sp) {
         char ch = sp.charAt(0);
