5c4b7ae1c7dac8ac1874093046d2f061effd7477
oiNeh <kgh22522252@gmail.com>
2022-07-14 23:31:31 +0900

    :package: 계산기 필터 테스트 import 수정

--- string-calculator/src/test/java/com/kata/calculation/CalculatorFilterTest.java
+++ string-calculator/src/test/java/com/kata/calculation/CalculatorFilterTest.java
@@ -1,5 +1,7 @@
 package com.kata.calculation;
 
+import com.kata.calculation.character.Character;
+import com.kata.calculation.numbers.Number;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
