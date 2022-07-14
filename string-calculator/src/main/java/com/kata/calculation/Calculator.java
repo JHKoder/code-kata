7c8657cb4aeb8af396759a167e7a123afebf03c4
oiNeh <kgh22522252@gmail.com>
2022-07-15 04:20:40 +0900

    :recycle: 계산기 입력에 look 추가

--- string-calculator/src/main/java/com/kata/calculation/Calculator.java
+++ string-calculator/src/main/java/com/kata/calculation/Calculator.java
@@ -2,10 +2,9 @@ package com.kata.calculation;
 
 public class Calculator {
 
-    public static String input(String str) {
+    public synchronized static String input(String str) {
         Calculation result = CalculatorFilter.of(str);
         return result.arithmetic();
     }
-
 
 }
