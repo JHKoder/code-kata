1810569873ad521412005468ead1473f1093c501
oiNeh <kgh22522252@gmail.com>
2022-07-18 23:27:29 +0900

    :white_check_mark: 문자열 계산기 연산 테스트 추가

---
+++ string-calculator/src/test/java/com/kata/calculation/OperationTest.java
@@ -0,0 +1,75 @@
+package com.kata.calculation;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("연산 테스트")
+public class OperationTest {
+
+
+    @Test
+    @DisplayName("더하기 성공")
+    void plusSuccess() {
+        //given
+        Integer left = 10;
+        String opera = "+";
+        Integer right = 2;
+
+        //when
+        Integer result = Operation.operation(left, opera, right);
+
+        //then
+        Assertions.assertThat(result)
+                .isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("뺴기 성공")
+    void findOutSuccess() {
+        //given
+        Integer left = 10;
+        String opera = "-";
+        Integer right = 2;
+
+        //when
+        Integer result = Operation.operation(left, opera, right);
+
+        //then
+        Assertions.assertThat(result)
+                .isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("나누기_성공")
+    void divideSuccess() {
+        //given
+        Integer left = 10;
+        String opera = "/";
+        Integer right = 2;
+
+        //when
+        Integer result = Operation.operation(left, opera, right);
+
+        //then
+        Assertions.assertThat(result)
+                .isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("곱셈_성공")
+    void multiplicationSuccess() {
+        //given
+        Integer left = 10;
+        String opera = "*";
+        Integer right = 2;
+
+        //when
+        Integer result = Operation.operation(left, opera, right);
+
+        //then
+        Assertions.assertThat(result)
+                .isEqualTo(20);
+    }
+}
