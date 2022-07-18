a147eb35d55e661d4c23ff1cdf23ee66ab1f0f83
oiNeh <kgh22522252@gmail.com>
2022-07-18 23:27:14 +0900

    :white_check_mark:  계산기 테스트 추가

---
+++ string-calculator/src/test/java/com/kata/calculation/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.kata.calculation;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@DisplayName("계산기 테스트")
+public class CalculatorTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1 + 2 + 3, 6",
+    })
+    @DisplayName("계산기 입력 정상")
+    void calculatorInputNormal(String str, Integer result) {
+        Calculator calculator = new Calculator();
+
+        Assertions.assertThat(calculator.input(str))
+                .isEqualTo(result);
+    }
+
+
+}
