103d5bbd44d597c65189ce160823cf7987a6637b
oiNeh <kgh22522252@gmail.com>
2022-07-20 03:09:43 +0900

    :recycle: 문자열 계산식 Pattern 제거 및 코드 리펙토링

--- string-calculator/src/main/java/com/kata/calculation/CalculatorExpression.java
+++ string-calculator/src/main/java/com/kata/calculation/CalculatorExpression.java
@@ -6,13 +6,9 @@ import static java.util.stream.Collectors.toCollection;
 import com.kata.exception.CalculatorException;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class CalculatorExpression {
-
-    private static final Pattern patternSymbol = Pattern.compile("[*/+-]");
-    private static final Pattern patternNumber = Pattern.compile("[0-9]{1,}");
 
     private final Queue<String> calculatorList = new LinkedList<>();
 
@@ -31,24 +27,16 @@ public class CalculatorExpression {
 
     private Queue<String> getOperationList() {
         return calculatorList.stream()
-                .filter(this::isSymbol)
+                .filter(ls -> ls.matches("[*/+-]"))
                 .collect(toCollection(LinkedList::new));
     }
 
     private Queue<Integer> getNumbersList() {
         return calculatorList.stream()
-                .filter(this::isNumber)
+                .filter(ls -> ls.matches("[0-9]{1,}"))
                 .flatMapToInt(ls -> IntStream.of(Integer.parseInt(ls)))
                 .boxed()
                 .collect(toCollection(LinkedList::new));
-    }
-
-    private boolean isSymbol(String str) {
-        return patternSymbol.matcher(str).matches();
-    }
-
-    private boolean isNumber(String str) {
-        return patternNumber.matcher(str).matches();
     }
 
 }
