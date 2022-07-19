d9163b26526d5534551966a1049b6cf6d7a2ec97
oiNeh <kgh22522252@gmail.com>
2022-07-20 01:41:25 +0900

    :recycle: 계산기 코드 수정

--- string-calculator/src/main/java/com/kata/calculation/Calculator.java
+++ string-calculator/src/main/java/com/kata/calculation/Calculator.java
@@ -1,28 +1,19 @@
 package com.kata.calculation;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toCollection;
+
+import java.util.LinkedList;
 
 public class Calculator {
 
     public Integer practice(String str) {
 
-        List<String> strings = Arrays.stream(str.split(" "))
-                .collect(Collectors.toUnmodifiableList());
+        CalculatorExpression calculatorExpression = new CalculatorExpression(stream(str.split(" "))
+                .collect(toCollection(LinkedList::new)));
 
-        List<String> operation = strings.stream()
-                .filter(Validation::isSymbol)
-                .collect(Collectors.toUnmodifiableList());
-
-        List<Integer> numbers = new ArrayList<>(strings.stream()
-                .filter(Validation::isNumber)
-                .flatMapToInt(ls -> IntStream.of(Integer.parseInt(ls)))
-                .boxed()
-                .collect(Collectors.toUnmodifiableList()));
-
-        return Operation.calculation(operation, numbers);
+        return calculatorExpression.calculation();
     }
+
+
 }
