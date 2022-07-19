9b068ac81fb3212f150d1022359faf1e4c1f54b8
oiNeh <kgh22522252@gmail.com>
2022-07-19 20:49:32 +0900

    :sparkles: 출력 기능 추가

---
+++ string-calculator/src/main/java/com/kata/calculation/Display.java
@@ -0,0 +1,15 @@
+package com.kata.calculation;
+
+public class Display {
+
+    private Integer value;
+
+    public Display(Integer value) {
+        this.value=value;
+    }
+
+    public void print() {
+        System.out.println(" 계산 값은 : "+value);
+    }
+
+}
