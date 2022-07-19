2f87246df322e58e6808bab89dc70bbd10b26546
oiNeh <kgh22522252@gmail.com>
2022-07-19 22:18:06 +0900

    :recycle: 계산기 검증 Pattern 코드 리펙토링

--- string-calculator/src/main/java/com/kata/calculation/Validation.java
+++ string-calculator/src/main/java/com/kata/calculation/Validation.java
@@ -3,17 +3,20 @@ package com.kata.calculation;
 import java.util.regex.Pattern;
 
 public class Validation {
+    private static final Pattern patternEquation = Pattern.compile("([\\d\\s]+[+*/-]+ [\\s\\d]){1,}");
+    private static final Pattern patternSymbol= Pattern.compile("[*/+-]");
+    private static final Pattern patternNumber= Pattern.compile("[0-9]");
 
     public static boolean formulaVerification(String str) {
-        return Pattern.matches("([\\d\\s]+[+*/-]+ [\\s\\d]){1,}", str);
+        return patternEquation.matcher(str).matches();
     }
 
     public static boolean isSymbol(String str){
-        return Pattern.matches("[*/+-]",str);
+        return patternSymbol.matcher(str).matches();
     }
 
     public static boolean isNumber(String str) {
-        return Pattern.matches("[0-9]",str);
+        return patternNumber.matcher(str).matches();
     }
 
 }
