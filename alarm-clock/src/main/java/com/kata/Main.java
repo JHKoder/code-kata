4e0e489482ef8851c577fb2b808523313563b7d9
oiNeh <kgh22522252@gmail.com>
2022-07-22 00:43:20 +0900

    :recycle: Main 코드 수정

--- alarm-clock/src/main/java/com/kata/Main.java
+++ alarm-clock/src/main/java/com/kata/Main.java
@@ -2,18 +2,22 @@ package com.kata;
 
 import com.kata.alarmclock.Alarm;
 import com.kata.alarmclock.AlarmClock;
+import com.kata.alarmclock.Battery;
 import com.kata.alarmclock.Time;
 
 public class Main {
 
     public static void main(String[] args) {
-        
-        Alarm alarm = new Alarm();
-        AlarmClock alarmClock = new AlarmClock(alarm);
 
-        alarm.add(new Time("20:44:00"));
+        Alarm alarm = new Alarm();
+        Battery battery = new Battery(100000);
+        AlarmClock alarmClock = new AlarmClock(alarm, battery);
+
+        alarm.add(new Time("0:20:10"));
         alarmClock.start();
-        alarm.add(new Time("23:44:00"));
+        alarm.add(new Time("0:35:50"));
+        alarm.add(new Time("0:36:20"));
+        alarm.add(new Time("0:36:30"));
 
     }
 
