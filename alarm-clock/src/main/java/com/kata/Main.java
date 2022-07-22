c3c333ce8912a960cfce876dca6f1ba8dc8c6e61
oiNeh <kgh22522252@gmail.com>
2022-07-22 10:27:57 +0900

    :recycle: Main 코드 리펙토링

--- alarm-clock/src/main/java/com/kata/Main.java
+++ alarm-clock/src/main/java/com/kata/Main.java
@@ -1,24 +1,14 @@
 package com.kata;
 
-import com.kata.alarmclock.Alarm;
 import com.kata.alarmclock.AlarmClock;
-import com.kata.alarmclock.Battery;
-import com.kata.alarmclock.Time;
 
 public class Main {
 
     public static void main(String[] args) {
+        AlarmClock alarmClock = new AlarmClock();
 
-        Alarm alarm = new Alarm();
-        Battery battery = new Battery(100000);
-        AlarmClock alarmClock = new AlarmClock(alarm, battery);
-
-        alarm.add(new Time("0:20:10"));
-        alarmClock.start();
-        alarm.add(new Time("0:35:50"));
-        alarm.add(new Time("0:36:20"));
-        alarm.add(new Time("0:36:30"));
-
+        alarmClock.alarm(9, 30, 1);
+        alarmClock.alarm(9, 30, 5);
     }
 
 }
