bd62873ad508c029d524b9dfdad48138b564b2c4
oiNeh <kgh22522252@gmail.com>
2022-07-23 05:02:27 +0900

    :recycle: 문자열 계산기 계산식 코드 리펙토링

--- string-calculator/src/main/java/com/kata/calculation/CalculatorExpression.java
+++ string-calculator/src/main/java/com/kata/calculation/CalculatorExpression.java
@@ -3,26 +3,25 @@ package com.kata.calculation;
 import static com.kata.calculation.Operation.operation;
 import static java.lang.Integer.parseInt;
 
-import java.util.LinkedList;
-import java.util.Objects;
 import java.util.Queue;
 
 public class CalculatorExpression {
 
-    private final Queue<String> calculatorList = new LinkedList<>();
+    private final Queue<String> calculatorList;
 
     public CalculatorExpression(Queue<String> str) {
-        calculatorList.addAll(str);
+        this.calculatorList = str;
     }
 
+    @SuppressWarnings("all")
     public Integer calculation() {
+        Integer sum = parseInt(calculatorList.poll());
 
-        Integer sum = parseInt(Objects.requireNonNull(calculatorList.poll()));
-
-        while(true){
-            if(calculatorList.peek() == null) return sum;
-            sum = operation(sum, calculatorList.poll(),  parseInt(Objects.requireNonNull(calculatorList.poll())));
+        while (!calculatorList.isEmpty()) {
+            sum = operation(sum, calculatorList.poll(), parseInt(calculatorList.poll()));
         }
+        return sum;
+
     }
 
 }
