59fda95b9cb26f6204c58007b6ae978492e3b01f
oiNeh <kgh22522252@gmail.com>
2022-07-22 21:41:26 +0900

    :recycle: Main 수정

--- string-calculator/src/main/java/com/kata/Main.java
+++ string-calculator/src/main/java/com/kata/Main.java
@@ -1,18 +1,12 @@
 package com.kata;
 
 import com.kata.calculation.Calculator;
-import com.kata.calculation.Display;
-import com.kata.io.BufferInput;
 
 public class Main {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-
-        BufferInput bufferInput = new BufferInput(calculator);
-        Display display = new Display(calculator.practice(bufferInput.input()));
-
-        display.print();
-        bufferInput.close();
+        calculator.practice();
     }
+    
 }
