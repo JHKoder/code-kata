d683dcd4d99103c71759996f89a4130a608d8790
oiNeh <kgh22522252@gmail.com>
2022-07-25 23:00:47 +0900

    :recycle: 시계 스케줄러 추가 코드

--- alarm-clock/src/main/java/com/kata/alarmclock/Clock.java
+++ alarm-clock/src/main/java/com/kata/alarmclock/Clock.java
@@ -1,24 +1,40 @@
 package com.kata.alarmclock;
 
-import java.time.LocalTime;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Clock {
+public class Clock extends Thread {
 
-    private final Time time;
+    private Time time;
+    Alarm alarm;
 
-    public Clock() {
-        LocalTime localTime = LocalTime.now();
-
-        this.time = new Time(localTime.getHour(), localTime.getMinute(), localTime.getSecond());
+    public Clock(Alarm alarm) {
+        time = new Time();
+        this.alarm = alarm;
     }
 
-    public Time timer() {
-        System.out.println(timerMessage());
-        return time.timeUp(LocalTime.now());
+    @Override
+    public void run() {
+        new Timer().scheduleAtFixedRate(new ScheduleTask(() -> {
+            time.secondUp();
+            alarm.checkAlarm(time);
+        }), new Date(), 1_000);
     }
 
-    public String timerMessage() {
-        return time.toString();
+    private static class ScheduleTask extends TimerTask {
+
+        private final Runnable runnable;
+
+        public ScheduleTask(Runnable runnable) {
+            this.runnable = runnable;
+        }
+
+        @Override
+        public void run() {
+            runnable.run();
+        }
+
     }
 
 }
