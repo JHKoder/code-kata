1e11dc7e6bb73faecd2ba4a725af82a51426ca39
oiNeh <kgh22522252@gmail.com>
2022-07-28 04:12:02 +0900

    :sparkles: 자오정 기능 추가

---
+++ korean-clock/src/main/java/code/kata/koreanclock/Meridian.java
@@ -0,0 +1,31 @@
+package code.kata.koreanclock;
+
+public class Meridian {
+
+    private final int hour;
+    private String[][] arr;
+
+    public Meridian(String[][] arr,int hour) {
+        this.arr=arr;
+        this.hour = hour;
+    }
+
+    public String[][] processing() {
+        if (hour == 0) {
+            arr[3][0] = parentheses(arr[3][0]);
+            arr[4][0] = parentheses(arr[4][0]);
+            return arr;
+        }
+        if (hour == 12) {
+            arr[4][0] = parentheses(arr[4][0]);
+            arr[5][0] = parentheses(arr[5][0]);
+            return arr;
+        }
+        return arr;
+    }
+
+    public String parentheses(String str) {
+        return "[" + str + "]";
+    }
+
+}
