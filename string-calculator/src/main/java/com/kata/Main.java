02614ccf2a7cfb2ff8ba0f5663fa528933d0a0dd
Kang JeoungHun <105915960+oiNeh@users.noreply.github.com>
2022-07-29 12:52:25 +0900

    :recycle: 문자열 계산기 수정 (#26)
    * :recycle: 문자열 계산기 코드 수정
    
    * :recycle: 문자열 계산기 표현 코드 수정
    
    * :white_check_mark: 문자열 계산기 유효성 검증 테스트
    
    * :recycle: Main 코드 수정

--- string-calculator/src/main/java/com/kata/Main.java
+++ string-calculator/src/main/java/com/kata/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.practice();
+        calculator.arithmetic();
     }
     
 }
--- string-calculator/src/main/java/com/kata/calculation/Calculator.java
+++ string-calculator/src/main/java/com/kata/calculation/Calculator.java
@@ -1,19 +1,14 @@
 package com.kata.calculation;
 
-import static java.util.Arrays.stream;
-import static java.util.stream.Collectors.toCollection;
-
 import com.kata.io.BufferInput;
-import java.util.LinkedList;
 
 public class Calculator {
 
-    public void practice() {
+    public void arithmetic() {
         BufferInput bufferInput = new BufferInput();
-        String str = bufferInput.input();
+        String inputCalculator = bufferInput.input();
 
-        CalculatorExpression calculatorExpression = new CalculatorExpression(stream(str.split(" "))
-                .collect(toCollection(LinkedList::new)));
+        CalculatorExpression calculatorExpression = new CalculatorExpression(inputCalculator);
 
         System.out.println(calculatorExpression.calculation());
         bufferInput.close();
