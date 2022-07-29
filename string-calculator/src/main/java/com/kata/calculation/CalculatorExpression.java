56d328a930a24205203f562c257f2b06a65fbea5
oiNeh <kgh22522252@gmail.com>
2022-07-29 12:50:38 +0900

    :recycle: 문자열 계산기 표현 코드 수정

--- string-calculator/src/main/java/com/kata/calculation/CalculatorExpression.java
+++ string-calculator/src/main/java/com/kata/calculation/CalculatorExpression.java
@@ -2,18 +2,21 @@ package com.kata.calculation;
 
 import static com.kata.calculation.Operation.operation;
 import static java.lang.Integer.parseInt;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toCollection;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class CalculatorExpression {
 
     private final Queue<String> calculatorList;
 
-    public CalculatorExpression(Queue<String> str) {
-        this.calculatorList = str;
+    public CalculatorExpression(String inputCalculator) {
+        calculatorList = stream(inputCalculator.split(" "))
+                .collect(toCollection(LinkedList::new));
     }
 
-    @SuppressWarnings("all")
     public Integer calculation() {
         Integer sum = parseInt(calculatorList.poll());
 
