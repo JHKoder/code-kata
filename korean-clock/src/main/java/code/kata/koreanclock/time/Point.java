1f5141210dde14cc843c6915d9d5b121604ebaac
oiNeh <kgh22522252@gmail.com>
2022-07-31 14:55:17 +0900

    :sparkles: Point 좌표 기능 추가

---
+++ korean-clock/src/main/java/code/kata/koreanclock/time/Point.java
@@ -0,0 +1,21 @@
+package code.kata.koreanclock.time;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point of(int x,int y){
+        return new Point(x,y);
+    }
+
+    public boolean isEquals(int x,int y){
+        return this.x == x && this.y == y;
+    }
+
+}
