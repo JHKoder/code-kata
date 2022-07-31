8c3bb001704930f1f8ba32ddaeb2d266f455d113
oiNeh <kgh22522252@gmail.com>
2022-07-31 14:54:32 +0900

    :recycle: 자오정 Enum 으로 리펙토링

---
+++ korean-clock/src/main/java/code/kata/koreanclock/time/MeridianEnum.java
@@ -0,0 +1,32 @@
+package code.kata.koreanclock.time;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum MeridianEnum {
+    MIDNIGHT(0,0, List.of(Point.of(3, 0), Point.of(4, 0))),
+    NOON(12,0, List.of(Point.of(5, 0), Point.of(4, 0)));
+
+    private int hour;
+    private int minute;
+    private List<Point> index;
+
+    MeridianEnum(int hour,int minute, List<Point> index) {
+        this.hour=hour;
+        this.minute=minute;
+        this.index=index;
+    }
+
+    public static boolean isMeridian(int hour,int minute){
+        return Arrays.stream(values())
+                .anyMatch( time -> time.hour == hour && time.minute ==minute);
+    }
+    public static List<Point>  findMeridian(int hour,int minute){
+        return Arrays.stream(values())
+                .filter(time -> time.hour == hour && time.minute == minute)
+                .findFirst()
+                .orElseThrow()
+                .index;
+    }
+
+}
