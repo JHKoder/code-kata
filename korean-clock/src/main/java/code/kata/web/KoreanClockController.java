fc522bbb989810b8cc54a432101e5755af92c0a3
oiNeh <kgh22522252@gmail.com>
2022-08-07 01:33:33 +0900

    :sparkles: 국가 uri 추가

---
+++ korean-clock/src/main/java/code/kata/web/KoreanClockController.java
@@ -0,0 +1,42 @@
+package code.kata.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class KoreanClockController {
+
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/kr")
+    public String krHome() {
+        return "index";
+    }
+    @GetMapping("/de")
+    public String deHome() {
+        return "index";
+    }
+    @GetMapping("/hk")
+    public String hkHome() {
+        return "index";
+    }
+    @GetMapping("/nz")
+    public String nzHome() {
+        return "index";
+    }
+    @GetMapping("/us")
+    public String usHome() {
+        return "index";
+    }
+    @GetMapping("/jp")
+    public String jpHome() {
+        return "index";
+    }
+
+}
