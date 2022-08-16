c926019e18b33b3047a9cb08c96acef619f76006
oiNeh <kgh22522252@gmail.com>
2022-08-16 22:33:47 +0900

    :sparkles: 알람 Pint 분리

---
+++ alarm-clock/src/main/java/com/kata/alarmclock/Pint.java
@@ -0,0 +1,25 @@
+package com.kata.alarmclock;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Pint {
+
+    private static final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public void print(String s) {
+        try {
+            out.write(s + "\n");
+            out.flush();
+        } catch (IOException ignored) {
+        }
+    }
+    public void shutdown(){
+        try {
+            out.close();
+        } catch (IOException ignored) {
+        }
+    }
+
+}
