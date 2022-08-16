b258b74dffaee2df49ff4a24c56e21525fa80886
Kang JeoungHun <kgh22522252@gmail.com>
2022-08-16 22:37:10 +0900

    :recycle: 알람시계 pint 코드 분리 후 리펙토링  (#35)
    * :sparkles: 알람 Pint 분리
    
    * :sparkles: 알람 Pint 추가하여 리펙토링
    
    * :sparkles: 알람 시계 코드 pint 추가
    
    * :recycle: 알람 출력 코드 리펙토링
    
    * :white_check_mark: 알람 pint 코드 추가로 인해 코드 수정

--- alarm-clock/src/main/java/com/kata/alarmclock/AlarmClock.java
+++ alarm-clock/src/main/java/com/kata/alarmclock/AlarmClock.java
@@ -2,12 +2,14 @@ package com.kata.alarmclock;
 
 public class AlarmClock extends Thread {
 
+    private Pint pint;
     private final Alarm alarm;
     private final Clock clock;
 
     public AlarmClock() {
-        alarm = new Alarm();
-        clock = new Clock(alarm);
+        pint = new Pint();
+        alarm = new Alarm(pint);
+        clock = new Clock(alarm,pint);
     }
 
     public void addTask(String hhMmSs) {
