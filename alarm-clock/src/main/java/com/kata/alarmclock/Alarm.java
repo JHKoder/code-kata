2d411d0f9cfce2b5b8592f3af524ef75f89f6ea7
oiNeh <kgh22522252@gmail.com>
2022-08-16 22:34:27 +0900

    :sparkles: 알람 Pint 추가하여 리펙토링

--- alarm-clock/src/main/java/com/kata/alarmclock/Alarm.java
+++ alarm-clock/src/main/java/com/kata/alarmclock/Alarm.java
@@ -3,29 +3,43 @@ package com.kata.alarmclock;
 import com.kata.alarmclock.exception.AlarmClockShutdownException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Timer;
 
 public class Alarm {
 
     private final Set<Time> timer = new HashSet<>();
+    private final Pint out;
+
+    public Alarm(Pint out) {
+        this.out = out;
+    }
 
     public void addTask(String alarmTime) {
         timer.add(new Time(alarmTime));
     }
 
-    public void checkAlarm(Time time) {
+    public void checkAlarm(Time time, Timer timers) {
 
         if (timer.size()==0) {
             throw new AlarmClockShutdownException();
         }
-        timer.removeIf(queue -> queueEqualsIf(queue, time));
+
+        try {
+            timer.removeIf(queue -> queueEqualsIf(queue, time));
+        }catch (UnsupportedOperationException e){
+            out.shutdown();
+            timers.cancel();
+        }
     }
 
     public boolean queueEqualsIf(Time queue, Time time) {
         if (queue.equals(time)) {
-            System.out.println("삐삐삐");
+            out.print("삐삐삐");
             return true;
         }
         return false;
     }
+
+
 
 }
