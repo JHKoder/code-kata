d44b5dee3ad83ab2704a17cb4f15f12a1a3afc6c
oineh <jeonghun.kang.dev@gmail.com>
2023-03-29 16:10:13 +0900

    :recycle: add Rock & Paper & Scissors equalsId

--- rock-paper-scissors/src/main/java/code/kata/rockPaperScissors/Factory.java
+++ rock-paper-scissors/src/main/java/code/kata/rockPaperScissors/Factory.java
@@ -4,13 +4,13 @@ import code.kata.domain.Player;
 
 public class Factory {
     public Hand apply(int number){
-        if(number == 1) return new Scissors();
-        if(number == 2) return new Rock();
-        if(number == 3) return new Paper();
+        if(Scissors.equalsId(number)) return new Scissors();
+        if(Rock.equalsId(number)) return new Rock();
+        if(Paper.equalsId(number)) return new Paper();
         throw new IllegalArgumentException("가위 바위 보 범위를 벗어 났습니다.");
     }
 
-    public void moveHand(Player user, Hand userCommand) {
-        user.updateHand(userCommand);
+    public void moveHand(Player player, Hand hand) {
+        player.updateHand(hand);
     }
 }
