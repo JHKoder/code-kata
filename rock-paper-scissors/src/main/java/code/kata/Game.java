32dd66f7a3ab718f8b07b87161bff1fb62b63895
oineh <jeonghun.kang.dev@gmail.com>
2023-03-30 01:04:46 +0900

    :recycle: refactor game code

---
+++ rock-paper-scissors/src/main/java/code/kata/Game.java
@@ -0,0 +1,33 @@
+package code.kata;
+
+
+import code.kata.console.ConsoleInput;
+import code.kata.console.ConsoleOutput;
+import code.kata.domain.Gamer;
+import code.kata.domain.Hand;
+
+import java.util.Random;
+
+public class Game {
+
+    public void play() {
+        ConsoleOutput output = new ConsoleOutput();
+
+        output.userInputMessage();
+
+        Gamer user = new Gamer(userCommand());
+        Gamer computer = new Gamer(Hand.valueOf(randomInt()));
+
+        output.resultMessage(user, computer);
+    }
+
+    public int randomInt() {
+        return new Random().nextInt(3) + 1;
+    }
+
+    private Hand userCommand() {
+        try (ConsoleInput inputAdapter = new ConsoleInput()) {
+            return Hand.valueOf(inputAdapter.input());
+        }
+    }
+}
