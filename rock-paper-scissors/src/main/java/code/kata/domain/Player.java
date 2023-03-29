7e8fd316dcb971d19f81386ca2503c19e832fca8
oineh <jeonghun.kang.dev@gmail.com>
2023-03-29 16:10:41 +0900

    :recycle: refactor Player static remove

--- rock-paper-scissors/src/main/java/code/kata/domain/Player.java
+++ rock-paper-scissors/src/main/java/code/kata/domain/Player.java
@@ -2,20 +2,8 @@ package code.kata.domain;
 
 
 import code.kata.rockPaperScissors.Hand;
-import code.kata.rockPaperScissors.Paper;
-import code.kata.rockPaperScissors.Rock;
-import code.kata.rockPaperScissors.Scissors;
 
 public interface Player {
-     Hand getHand();
-
-     static Hand init(int number) {
-          if(number == 1)
-               return new Scissors();
-          if (number == 2)
-               return new Rock();
-          return new Paper();
-     }
-
-     void updateHand(Hand hand);
+    Hand getHand();
+    void updateHand(Hand hand);
 }
