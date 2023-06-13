1021938d13fa03f5f723a1a88b173ec611c04db9
oineh <jeonghun.kang.dev@gmail.com>
2023-06-13 18:22:47 +0900

    :recycle: update hand

--- rock-paper-scissors/src/main/java/code/kata/domain/Hand.java
+++ rock-paper-scissors/src/main/java/code/kata/domain/Hand.java
@@ -14,16 +14,24 @@ public enum Hand {
     }
 
     public static Hand valueOf(int index) {
-        return Arrays.stream(values()).filter(hand -> hand.index == index).findFirst()
+        return Arrays.stream(values())
+                .filter(hand -> hand.index == index).findFirst()
                 .orElseThrow();
     }
 
     public Result fight(Hand hand) {
-        if ((this.index % 3) + 1 == hand.index) {
+        if (isLose(hand)) {
             return Result.LOSE;
-        } else if (this.index == hand.index) {
+        }else if (isDraw(hand)) {
             return Result.DRAW;
-        }
-        return Result.WIN;
+        } return Result.WIN;
+    }
+
+    private boolean isLose(Hand hand){
+        return (this.index % 3) + 1 == hand.index;
+    }
+
+    private boolean isDraw(Hand hand){
+        return this.index == hand.index;
     }
 }
